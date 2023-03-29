185dde6d328b38620802e5722636bb99494cca1d
babafemiolatona <femiolatona@gmail.com>
2023-03-29 11:07:48 -0700

    Added 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string
+ *
+ * @dest: First input parameter
+ *
+ * @src: Second input parameter
+ *
+ * @n: Third input parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i++] = '\0';
+	}
+	return (dest);
+}
