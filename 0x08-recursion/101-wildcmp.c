d63cb49920890f2cc24a7606c832376e533e89b8
babafemiolatona <femiolatona@gmail.com>
2023-04-05 17:35:27 -0700

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,39 +1,75 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1
- * if they can be considered identical, otherwise returns 0
- *
- * @s1: the first string to compare
- * @s2: the second string to compare
- *
- * Return: 1 if the strings can be considered identical, 0 otherwise
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
+ * Return: Always 0.
  */
+int checker(char *s1, char *s2, int a, int b, int wildUsed);
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '\0')
-		{
-			return (1);
-		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
+return (checker(s1, s2, 0, 0, -1));
+}
+
+/**
+ * checkLast - check last char of s2 when s1 ends
+ * @s: string
+ * @i: int
+ * Return: 0 or 1
+ */
+int checkLast(char *s, int i)
+{
+if (s[i] == '*')
+	return (checkLast(s, i + 1));
+else if (s[i] == '\0')
+	return (1);
+
+return (0);
+
+}
+/**
+ * checker - helper
+ * @s1: string
+ * @s2: string
+ * @a: int
+ * @b: int
+ * @wildUsed: int
+ * Return: Always 0.
+ */
+int checker(char *s1, char *s2, int a, int b, int wildUsed)
+{
+
+if (s1[a] != '\0')
+{
+	if (s2[b] == '\0')
 		return (0);
-	}
-	if (*s1 == '\0')
+	else if (s2[b] == '*')
 	{
-		return (*s2 == '\0');
+		if (s2[b + 1] == '*')
+			return (checker(s1, s2, a, b + 1, b));
+		else if (s2[b + 1] == s1[a])
+			return (checker(s1, s2, a, b + 1, b));
+		else if (s1[a + 1] != s2[b + 1])
+			return (checker(s1, s2, a + 1, b, b));
+		else if (s1[a + 1] == s2[b + 1])
+			return (checker(s1, s2, a + 1, b + 1, b));
 	}
-	if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	return (0);
+	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
+		return (checker(s1, s2, a + 1, b + 1, wildUsed));
+
+	if (wildUsed == -1)
+		return (0);
+
+	return (checker(s1, s2, a, wildUsed, wildUsed));
+
+}
+if (s2[b] != '\0')
+	return (checkLast(s2, b));
+
+return (1);
+
 }
