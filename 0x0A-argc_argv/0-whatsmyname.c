c2832deff1297a19dbf3d0b16610d7daa7432060
babafemiolatona <femiolatona@gmail.com>
2023-04-07 20:06:21 +0200

    Added 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the strings containing the arguments
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return 0;
+}
