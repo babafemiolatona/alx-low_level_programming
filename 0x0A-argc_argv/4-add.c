6806107361cd3028a4c382d0000180c6bc41e2c4
babafemiolatona <femiolatona@gmail.com>
2023-04-07 22:17:50 +0200

    Added 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments passed to the program
+ * @argv: array of pointers to the strings containing the arguments
+ * Return: 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		char *p = argv[i];
+		for (; *p != '\0'; p++)
+		{
+			if (!isdigit(*p))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
