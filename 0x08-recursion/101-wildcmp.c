67e9dc60cc9ed1cef0ca47c8dd55b5a28717b584
babafemiolatona <femiolatona@gmail.com>
2023-04-10 01:12:36 +0200

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * wildcmp - check the code for Holberton School students.
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
