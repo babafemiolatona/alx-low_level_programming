fa41c77dfe8fec7014149760c6fa15b7d987f804
babafemiolatona <femiolatona@gmail.com>
2023-04-14 00:51:57 +0200

    Update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,37 +1,59 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _memset - copy values of ptr to mem
+ * up until min
+ *
+ * @mem: new memory address (dest)
+ * @ptr: old memory address (src)
+ * @min: min number of copy
+ */
+void _memset(char *mem, char *ptr, int min)
+{
+	while (min > 0)
+	{
+		*mem = *ptr;
+		mem++;
+		ptr++;
+		min--;
+	}
+}
 
 /**
  * _realloc - reallocates a memory block
+ * using malloc and free
  *
- * @ptr: pointer to the memory previously allocated
- * @old_size: size in bytes of the allocated space for ptr
- * @new_size: new size in bytes of the new memory block
+ * @ptr: pointer for old address
+ * @old_size: old size of memory address ptr
+ * @new_size: new size to be reallocated
  *
- * Return: new_ptr
+ * Return: newly allocated memory address if
+ * successfull
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	unsigned int min = old_size;
+	char *mem;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (old_size == new_size)
+		return (ptr);
 
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	new_ptr = malloc(new_size);
 
-	if (ptr == NULL)
-	{
-		return (new_ptr);
-	}
-	if (new_ptr == NULL)
-	{
-		return (NULL);
-	}
-	memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+	if (new_size < min)
+		min = new_size;
+
+	mem = malloc(new_size);
+	_memset(mem, ptr, min);
 	free(ptr);
-	return (new_ptr);
+
+	return (mem);
 }
