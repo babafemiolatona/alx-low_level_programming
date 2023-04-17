324126d9c119557c03ea3b34f595173bcea2507d
babafemiolatona <femiolatona@gmail.com>
2023-04-17 21:28:52 +0200

    Added header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: pointer to character array(string)
+ * @age: integer
+ * @owner: pointer to character array(string)
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
