540de660ac511428b29819cec80a06b9f1fe9a59
babafemiolatona <femiolatona@gmail.com>
2023-05-01 17:12:47 +0200

    Debugging

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
+	if (head == NULL)
+		return;
 
-	while (*head != NULL)
+	if (*head == NULL)
 	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
+		free(*head);
+		*head = NULL;
+		return;
 	}
+
+	free_listint2(&((*head)->next));
+	free(*head);
 	*head = NULL;
 }
