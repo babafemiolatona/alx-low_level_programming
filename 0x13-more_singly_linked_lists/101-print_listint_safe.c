37dd32f5e5032e8f9b97ead698e3c9c80a02d817
babafemiolatona <femiolatona@gmail.com>
2023-05-02 18:05:18 +0200

    Refactored code

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,31 +1,71 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list, even if it has a loop
+ * __p - prints no looped list.
  *
- * @head: pointer to the head of the listint_t list
+ * @head: pointer to the start of the list
  *
- * Return: number of nodes in the list
+ * Return: the number of nodes in the list
  */
-
-size_t print_listint_safe(const listint_t *head)
+size_t __p(const listint_t *head)
 {
+	const listint_t *slow = head;
 	size_t num = 0;
-	long int diff;
 
-	while (head)
+	while (slow != NULL)
 	{
-		diff = head - head->next;
 		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
 	}
 
+	return (num);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+	size_t num = 0;
+	int count = 0;
+
+	if (head == NULL)
+		return (0);
+	if (head->next == NULL)
+		return (1);
+	do {
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			break;
+	} while (fast && fast->next);
+	if (slow != fast)
+		return (__p(head));
+
+	slow = head;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	slow = head;
+	if (slow == fast)
+		count++;
+	while (count <= 1)
+	{
+		num++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		if (slow == fast)
+			count++;
+	}
+	printf("-> [%p] %d\n", (void *)fast, fast->n);
 	return (num);
 }
