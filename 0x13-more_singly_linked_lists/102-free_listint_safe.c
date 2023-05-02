4504a191cd2ea13fb9282692793eff19b03a8cc9
babafemiolatona <femiolatona@gmail.com>
2023-05-02 18:18:44 +0200

    Refactored code

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,95 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list safely
+ * __free - free a node
  *
- * @h: pointer to the head of the list
+ * @h: node
  *
- * Return: The size of the list that was freed.
- * The function sets the head to NULL
+ * Return: size of node
  */
+size_t __free(listint_t **h)
+{
+	free(*h);
+	*h = NULL;
+	return (1);
+}
 
+/**
+ * __f - free normal linked list
+ *
+ * @h: node
+ *
+ * Return: size of freed list
+ */
+size_t __f(listint_t **h)
+{
+	if (*h == NULL)
+		return (0);
+
+	return (__f(&((*h)->next)) + __free(h));
+}
+
+/**
+ * __safe_free - free circular
+ * linked list
+ *
+ * @h: node
+ * @loop: node
+ * @count: count
+ *
+ * Return: size of freed list
+ */
+size_t __safe_free(listint_t **h, listint_t *loop, int count)
+{
+	if (*h == loop)
+	{
+		if (count == 2)
+			return (0);
+		count++;
+	}
+
+	return (__safe_free(&((*h)->next), loop, count) + __free(h));
+}
+
+/**
+ * free_listint_safe - free linked list
+ *
+ * @h: node
+ *
+ * Return: size of freed list
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *temp;
+	listint_t *slow = *h;
+	listint_t *fast = *h;
+	int count = 0;
 
-	if (h == NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	if ((*h)->next == NULL)
+		return (__f(h));
+
+	do {
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			break;
+	} while (fast && fast->next);
+
+	if (slow != fast)
+		return (__f(h));
+
+	slow = *h;
+	while (slow != fast)
 	{
-		return (size);
+		slow = slow->next;
+		fast = fast->next;
 	}
 
-	while (*h != NULL)
-	{
-		if (*h <= (*h)->next)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-		}
-		size++;
-	}
-	*h = NULL;
-	return (size);
+	if (slow == fast)
+		count++;
+
+	return (__safe_free(h, fast, count));
 }
