91eec48555cb60ab0260c3d2d1e95d5409f38494
babafemiolatona <femiolatona@gmail.com>
2023-05-04 16:32:38 +0200

    Updated header file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of
+ * an unsigned long integer
+ *
+ * @n: number to print in binary format
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) ? '1' : '0');
+}
